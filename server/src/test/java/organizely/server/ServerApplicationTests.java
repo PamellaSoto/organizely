8a855af4747787fe1eac1f713dfba960ebd078d5
Pamella Sotomayor <pamellasoto.dev@gmail.com>
2025-12-06 00:06:24 -0300

    fix(docker): add alternative path

--- server/src/test/java/organizely/server/ServerApplicationTests.java
+++ server/src/test/java/organizely/server/ServerApplicationTests.java
@@ -2,8 +2,9 @@ package organizely.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import organizely.app.ServerApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = ServerApplication.class)
 class ServerApplicationTests {
 
 	@Test
